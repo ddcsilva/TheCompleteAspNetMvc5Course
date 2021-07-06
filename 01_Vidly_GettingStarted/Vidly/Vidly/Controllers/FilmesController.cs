7a97c4dee3743f48b961d2770fcb413d16a88c39
Danilo Silva <danilo.silva@msn.com>
2021-07-06 20:32:20 -0300

    MVC in Action

---
+++ 01_Vidly_GettingStarted/Vidly/Vidly/Controllers/FilmesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class FilmesController : Controller
+    {
+        // GET: Filmes/Aleatorio
+        public ActionResult Aleatorio()
+        {
+            var filme = new Filme()
+            {
+                Nome = "Shrek"
+            };
+
+            return View();
+        }
+    }
+}
