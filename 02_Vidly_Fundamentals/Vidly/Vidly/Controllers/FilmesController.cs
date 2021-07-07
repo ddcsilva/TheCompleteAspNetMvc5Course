4625bf29877a601c5ccfd61cbe36470cd523000c
Danilo Silva <danilo.silva@msn.com>
2021-07-06 23:21:13 -0300

    Testing Action Results

---
+++ 02_Vidly_Fundamentals/Vidly/Vidly/Controllers/FilmesController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class FilmesController : Controller
+    {
+        // GET: Filmes/Aleatorio
+        public ActionResult Aleatorio()
+        {
+            var filme = new Filme()
+            {
+                Nome = "Shrek"
+            };
+
+            // Retorna a View tipada
+            //return View(filme);
+
+            // Retorna um texto simples
+            //return Content("Hello World!");
+
+            // Retorna um HTTP Code
+            //return HttpNotFound();
+
+            // Semelhante ao void
+            //return new EmptyResult();
+
+            // Retorna um redirecionamento com parâmetros na query string
+            return RedirectToAction("Index", "Home", new { pagina = 1, filtro = "nome" });
+        }
+    }
+}
