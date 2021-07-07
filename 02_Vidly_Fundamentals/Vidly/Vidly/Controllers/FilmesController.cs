fa38e4d3c3ae4a6ce19ec1caddb3592651bd477c
Danilo Silva <danilo.silva@msn.com>
2021-07-06 23:34:34 -0300

    Action Parameters

--- 02_Vidly_Fundamentals/Vidly/Vidly/Controllers/FilmesController.cs
+++ 02_Vidly_Fundamentals/Vidly/Vidly/Controllers/FilmesController.cs
@@ -18,7 +18,7 @@ namespace Vidly.Controllers
             };
 
             // Retorna a View tipada
-            //return View(filme);
+            return View(filme);
 
             // Retorna um texto simples
             //return Content("Hello World!");
@@ -30,7 +30,28 @@ namespace Vidly.Controllers
             //return new EmptyResult();
 
             // Retorna um redirecionamento com parâmetros na query string
-            return RedirectToAction("Index", "Home", new { pagina = 1, filtro = "nome" });
+            //return RedirectToAction("Index", "Home", new { pagina = 1, filtro = "nome" });
+        }
+
+        // A Rota padrão é definida em App_Start -> RouteConfig.cs (O padrão é o parâmetro "id" opcional)
+        public ActionResult Editar(int id)
+        {
+            return Content("id = " + id);
+        }
+
+        // Se for necessário novos parâmetros, adicione parâmetros opcionais na action e faça as atribuições necessárias após a Request
+        public ActionResult Index(int? pagina, string filtro)
+        {
+            if (!pagina.HasValue)
+            {
+                pagina = 1;
+            }
+
+            if (String.IsNullOrWhiteSpace(filtro)) {
+                filtro = "Nome";
+            }
+
+            return Content(String.Format("pagina={0}&filtro={1}", pagina, filtro));
         }
     }
 }
